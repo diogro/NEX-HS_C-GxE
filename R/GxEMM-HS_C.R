2112d2150a1365b58829d54ac861408e460cca35
Diogo Melo <diogro@gmail.com>
2021-06-17 13:48:33 -0400

    GxEmm script

--- R/GxEMM-HS_C.R
+++ R/GxEMM-HS_C.R
@@ -1,9 +1,9 @@
 library(GxEMM)
-ldak_loc  <- "~/.local/bin/ldak5.linux "
+ldak_loc  <- "/Genomics/grid/users/damelo/.local/bin/ldak5.linux "
 
 library(plyr)
 library(doMC)
-registerDoMC(32)
+registerDoMC(64)
 
 expr_df = t(read.table("/Genomics//ayroleslab2/lamaya//bigProject//Datasets//GXEpaper//GeneCounts/VOOMCounts_CPM1_head_hsctrl_covfree_4svs_CORRECT_Jan8.21.txt",
                        row.names = 1))
@@ -21,6 +21,13 @@ X     <- Z_all[,-1] # fixed effect covariates. Must include Z! Column is dropped
 
 GRM = as.matrix(read.table("/Genomics//ayroleslab2/lamaya//bigProject//Datasets//GXEpaper/GRM/head.hsc.final.norelatedness1.miss50.sXX.txt"))
 
-out_free_HSC = llply(1:32, function(i) GxEMM(expr_df[,i], X = X, K = GRM, Z = Z_all, 
+
+
+out_free_HSC = llply(colnames(expr_df), function(i) GxEMM(expr_df[,i], X = X, K = GRM, Z = Z_all, 
                                              gtype='free', etype='free', ldak_loc=ldak_loc),
                      .parallel = TRUE)
+
+saveRDS(out_free_HSC, file = "/Genomics/ayroleslab2/diogro/projects/NEX-HS_C-GxE/data/output/GxEmm_all_genes_outlist.rds")
+
+
+  
