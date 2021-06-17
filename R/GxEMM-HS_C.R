4d51f07b73df56bb7115a6b08a3cdb69fc8c8ac3
Diogo Melo <diogro@gmail.com>
2021-06-17 14:07:21 -0400

    paths maybe

--- R/GxEMM-HS_C.R
+++ R/GxEMM-HS_C.R
@@ -5,11 +5,11 @@ library(plyr)
 library(doMC)
 registerDoMC(64)
 
-expr_df = t(read.table("/Genomics//ayroleslab2/lamaya//bigProject//Datasets//GXEpaper//GeneCounts/VOOMCounts_CPM1_head_hsctrl_covfree_4svs_CORRECT_Jan8.21.txt",
+expr_df = t(read.table("/Genomics/ayroleslab2/lamaya/bigProject/Datasets/GXEpaper/GeneCounts/VOOMCounts_CPM1_head_hsctrl_covfree_4svs_CORRECT_Jan8.21.txt",
                        row.names = 1))
 rownames(expr_df) = gsub("^X", "", rownames(expr_df))
 
-covariates = read.table("/Genomics//ayroleslab2/lamaya//bigProject//Datasets//GXEpaper/Covariates_forGEMMA_Jan82021.txt")
+covariates = read.table("/Genomics/ayroleslab2/lamaya//bigProject/Datasets/GXEpaper/Covariates_forGEMMA_Jan82021.txt")
 ID_C = row.names(covariates[covariates$treatment == 1,])
 ID_HS = row.names(covariates[covariates$treatment == 2,])
 
@@ -17,9 +17,9 @@ Z_HS = as.numeric(rownames(expr_df) %in% ID_HS)
 Z_C = as.numeric(rownames(expr_df) %in% ID_C)
 
 Z_all = cbind(Z_HS, Z_C)
-X     <- Z_all[,-1] # fixed effect covariates. Must include Z! Column is dropped here so that cbind(1,X) is full rank
+X     <- Z_all[,-1] 
 
-GRM = as.matrix(read.table("/Genomics//ayroleslab2/lamaya//bigProject//Datasets//GXEpaper/GRM/head.hsc.final.norelatedness1.miss50.sXX.txt"))
+GRM = as.matrix(read.table("/Genomics/ayroleslab2/lamaya/bigProject/Datasets/GXEpaper/GRM/head.hsc.final.norelatedness1.miss50.sXX.txt"))
 
 
 
