2aed687f6940c5bb4086a761c12d11f5dc22387b
Diogo Melo <diogro@gmail.com>
2021-08-19 11:06:23 -0400

    new analysis of SBM outputs

--- R/graph_tool_analysis_HeadBody.R
+++ R/graph_tool_analysis_HeadBody.R
@@ -1,40 +1,71 @@
 if(!require(plyr)){install.packages("plyr"); library(plyr)}
 if(!require(tidyverse)){install.packages("tidyverse"); library(tidyverse)}
+if(!require(cowplot)){install.packages("cowplot"); library(cowplot)}
+if(!require(ggthemes)){install.packages("ggthemes"); library(ggthemes)}
+if(!require(xkcd)){install.packages("xkcd"); library(xkcd)}
+if(!require(extrafont)){install.packages("extrafont"); library(extrafont)}
 if(!require(superheat)){install.packages("superheat"); library(superheat)}
 if(!require(mcclust)){install.packages("mcclust"); library(mcclust)}
+if(!require(patchwork)){install.packages("patchwork"); library(patchwork)}
 
-head = read_csv("data/output/4273genes-conectivity_30-non_hierarchical_sbm-fit_head_df.csv")
-body = read_csv("data/output/6176genes-non_hierarchical_sbm-fit_body_df.csv")
+head_files = dir("data/output/SBM/clustering/", pattern = "head_cutoff-spearman_val-", full.names = T)
+head_labels_raw = dir("data/output/SBM/clustering/", pattern = "head_cutoff-spearman_val-")
 
-head$tissue = "head"
-body$tissue = "body"
+body_files = dir("data/output/SBM/clustering/", pattern = "body_cutoff-spearman_val-", full.names = T)
+body_labels_raw = dir("data/output/SBM/clustering/", pattern = "body_cutoff-spearman_val-")
 
-head = dplyr::semi_join(head, body, by = "Gene")
-body = dplyr::semi_join(body, head, by = "Gene")
-head = dplyr::semi_join(head, body, by = "Gene")
-body = dplyr::semi_join(body, head, by = "Gene")
 
-table(head$Block) |> length()
-table(body$Block) |> length()
+readClusteringFolder <- function(tissue_files, tissue_labels_raw) {
+  tissue_labels_df = data.frame(cut0ff = sapply(str_split(tissue_labels_raw,"_"), function(x) str_split(x[[3]], "-")[[1]][2]),
+             type = sapply(str_split(tissue_labels_raw,"_"), function(x) str_split(x[[4]], "-")[[1]][1])) %>%
+    mutate(type =  ifelse(type == "non", "non.nested", "nested"),
+           label = paste(type, cut0ff, sep = "."))
+  
+  readClustering = function(file){
+    x = read_csv(file) %>%
+      mutate_at(vars(matches("B")), ~as.numeric(factor(.)))
+  }
+  
+  tissue_labels_df$cut0ff
+  tissue_clusters_raw = lapply(tissue_files, readClustering)
+  tissue_clusters = list(nested = tissue_clusters_raw[tissue_labels_df$type == "nested"],
+                         non.nested =  tissue_clusters_raw[tissue_labels_df$type == "non.nested"])
+  names(tissue_clusters[['nested']]) = tissue_labels_df$cut0ff[tissue_labels_df$type == "nested"]
+  names(tissue_clusters[['non.nested']]) = tissue_labels_df$cut0ff[tissue_labels_df$type == "non.nested"]
+  
+  tissue_clusters
+}
 
-body_expr = t(read.table("./data/VOOMCounts_CPM1_body_ctrl_Jun2.20.txt",row.names = 1))
-body_expr = body_expr[,body$Gene]
-body_expr = as_tibble(body_expr)
+head_clusters = readClusteringFolder(head_files, head_labels_raw)
+body_clusters = readClusteringFolder(body_files, body_labels_raw)
 
-head_expr = t(read.table("./data/VOOMCounts_CPM1_head_ctrl_Jun2.20.txt",row.names = 1))
-head_expr = head_expr[,head$Gene]
-head_expr = as_tibble(head_expr)
+n_clusters_df = rbind(
+  data.frame(n_clusters = sapply(head_clusters[['non.nested']], function(x) length(unique((x$Block)))),
+             cutOff = as.numeric(names(head_clusters[['non.nested']])),
+             type = "non.nested", tissue = "head"),
+  data.frame(n_clusters = sapply(head_clusters[['nested']], function(x) length(unique((x$B1)))),
+             cutOff = as.numeric(names(head_clusters[['nested']])),
+             type = "nested", tissue = "head") ,
+  data.frame(n_clusters = sapply(body_clusters[['non.nested']], function(x) length(unique((x$Block)))),
+             cutOff = as.numeric(names(body_clusters[['non.nested']])),
+             type = "non.nested", tissue = "body"  ),
+  data.frame(n_clusters = sapply(body_clusters[['nested']], function(x) length(unique((x$B1)))),
+             cutOff = as.numeric(names(body_clusters[['nested']])),
+             type = "nested", tissue = "body")  
+                )
 
-cor_head = cor(head_expr)
-cor_body = cor(body_expr)
+n_clusters_plot = plot_grid(n_clusters_df %>%
+  filter(tissue == "head") %>%
+  ggplot(aes(cutOff, n_clusters, groups = type, color= type)) + 
+    geom_line(size = 1) + 
+    theme_tufte() + background_grid() + theme(legend.position = "top") +
+    ggtitle("Number of clusters - Head - Nested vs non-nested"),
+n_clusters_df %>%
+  filter(tissue == "body") %>%
+  ggplot(aes(cutOff, n_clusters, groups = type, color= type)) + 
+  geom_line(size = 1) + 
+  theme_tufte() + background_grid() + theme(legend.position = "top") +
+  ggtitle("Number of clusters - Body - Nested vs non-nested"), ncol = 2)
 
-superheat(cor_head, 
-          membership.rows = head$Block, 
-          membership.cols = head$Block, 
-          smooth.heat = TRUE)
-superheat(cor_body, 
-          membership.rows = body$Block, 
-          membership.cols = body$Block, 
-          smooth.heat = TRUE)
-
-vi.dist(head$Block, body$Block, parts = TRUE)
+save_plot(filename = "data/output/SBM/plots/number_of_gene_clusters_nested-vs-non-nested.png", 
+          n_clusters_plot, base_height = 5, ncol = 2, base_asp = 1.3)
