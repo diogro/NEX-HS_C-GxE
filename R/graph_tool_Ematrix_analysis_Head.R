be65a5e16e9356766e1e61152ace672eadbba37e
Diogo Melo <diogro@gmail.com>
2021-09-02 12:38:34 -0400

    more better functions for E matrices

--- R/graph_tool_Ematrix_analysis_Head.R
+++ R/graph_tool_Ematrix_analysis_Head.R
@@ -8,39 +8,48 @@ if(!require(superheat)){install.packages("superheat"); library(superheat)}
 if(!require(mcclust)){install.packages("mcclust"); library(mcclust)}
 if(!require(patchwork)){install.packages("patchwork"); library(patchwork)}
 
-block_df = read_csv("data/output/SBM/clustering/head_mcmc_cutoff-spearman_val-0.5_hierarchical-SBM.csv")
+header = "head_mcmc_10000_cutoff-spearman_val-0.4"
+#header = "head_mcmc_cutoff-spearman_val-0.5"
 
-block_df = block_df %>%
-  arrange(B5, B4, B3, B2, B1, desc(Degree))
-
-e_mats = vector("list", 5)
-for (i in seq_along(e_mats)){
-  e_matrix = read_csv(paste0("data/output/SBM/clustering/head_mcmc_cutoff-spearman_val-0.5_hierarchical-SBM_e_matrix_level",i-1,".csv"))
-  e_matrix$X1 = NULL
-  e_matrix = as.matrix(e_matrix)
-  rownames(e_matrix) = colnames(e_matrix) = as.character(colnames(e_matrix))
-  blocks = as.character(unique(as.data.frame(block_df)[,paste0("B", i)]))
-  e_matrix= e_matrix[blocks, blocks]
-  #e_matrix = e_matrix[-61, -61]
-  diag(e_matrix) = diag(e_matrix)/2
-  e_mats[[i]] = e_matrix
+makeEmatrixPlots = function(header, 
+                            data_path = "data/output/SBM/clustering/", 
+                            plot_path = "data/output/SBM/plots/",
+                            levels = 5){
+  block_df = read_csv(file.path(data_path, paste0(header, "_hierarchical-SBM.csv")))
+  
+  block_df = block_df %>%
+    arrange(B6, B5, B4, B3, B2, B1, desc(Degree))
+  
+  e_mats = vector("list", levels)
+  for (i in seq_along(e_mats)){
+    e_matrix = read_csv(file.path(data_path, paste0(header, "_hierarchical-SBM_e_matrix_level",i-1,".csv")))
+    e_matrix$X1 = NULL
+    e_matrix = as.matrix(e_matrix)
+    rownames(e_matrix) = colnames(e_matrix) = as.character(colnames(e_matrix))
+    blocks = as.character(unique(as.data.frame(block_df)[,paste0("B", i)]))
+    e_matrix= e_matrix[blocks, blocks]
+    #e_matrix = e_matrix[-61, -61]
+    diag(e_matrix) = diag(e_matrix)/2
+    e_mats[[i]] = e_matrix
+  }
+  
+  
+  makePlot = function(e_matrix){
+    colnames(e_matrix) = rownames(e_matrix) = paste0("b", rownames(e_matrix))
+    melted_cormat <- reshape2::melt(e_matrix)
+    melted_cormat[melted_cormat == 0] = NA
+    #plot heatmap
+    ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
+      geom_tile()+
+      scale_fill_viridis_c(alpha = 1)  + theme_tufte() + labs(y = "Blocks", x = "Blocks") +
+      theme(axis.text.x = element_text(angle=45, vjust=0.6), legend.position = "none") 
+  }
+  plot_list = lapply(e_mats, makePlot)
+  
+  all_plots = plot_grid(plotlist = plot_list)
+  save_plot(file.path(plot_path, paste0(header, "_E_matrices.png")), all_plots, 
+            base_height = 10, base_asp = 1.2, ncol = 3, nrow = 2)
+  return(list(block_df, e_mats, all_plots))
 }
-
-
-makePlot = function(e_matrix){
-  colnames(e_matrix) = rownames(e_matrix) = paste0("b", rownames(e_matrix))
-  melted_cormat <- reshape2::melt(e_matrix)
-  melted_cormat[melted_cormat == 0] = NA
-  #plot heatmap
-  ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
-    geom_tile()+
-    scale_fill_viridis_c(alpha = 1)  + theme_tufte() + labs(y = "Blocks", x = "Blocks") +
-    theme(axis.text.x = element_text(angle=45, vjust=0.6), legend.position = "none") 
-}
-plot_list = lapply(e_mats, makePlot)
-
-all_plots = plot_grid(plotlist = plot_list)
-save_plot("data/output/SBM/plots/E_matrices.png", all_plots, 
-          base_height = 5, base_asp = 1.2, ncol = 3, nrow = 2)
-
-
+makeEmatrixPlots("head_mcmc_10000_cutoff-spearman_val-0.4", levels = 5)
+makeEmatrixPlots("head_mcmc_cutoff-spearman_val-0.5")
