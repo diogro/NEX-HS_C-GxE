f033777b7aaba20d3e197a947ac582aac6862fe4
Diogo Melo <diogro@gmail.com>
2021-09-29 14:42:36 -0400

    lot's of GO tools

--- R/GO_tests.R
+++ R/GO_tests.R
@@ -15,15 +15,19 @@ flyGO = readRDS("org.Dm.egGOBP.2021-09-27")
 #BiocManager::install("clusterProfiler")
 library(clusterProfiler)
 
-
 getLevel = function(x, n_levels = 4){
   x = gsub(".csv", "", x)
   x = gsub("/", "", x)
   n_levels - length(strsplit(x, "-")[[1]]) + 1
 }
-block_path = "data/output/SBM/clustering/body_weights-spearman_fdr-1e-05_mcmc_mode_hierarchical-SBM_gene-blocks"
-level = 1
-block  = 11
+getChild = function(parent, b_df = block_summary){
+  if(!parent %in% b_df$Name) stop("Parent block not found.")
+  childs = b_df %>%
+    filter(Nested_Level == getLevel(parent)-1, Parent == strsplit(parent, "-")[[1]][1]) %>%
+    as.data.frame %>% `[[`("Name")
+  c(parent, childs)
+}
+
 getGeneList = function(block_number, level, folder_path, file_name = NULL){
   if(is.null(file_name)){
     file_path = file.path(folder_path, paste0("Level_", level))
@@ -65,7 +69,59 @@ getEnrichment = function(block_number = NULL, level = NULL,
                          ontology.algorithm = "none")
   enGo
 }
+XGR_plot = function(x){
+  l = enGo_XGR[getChild(x)]
+  l = l[!sapply(l, is.null)]
+  if(length(l) > 2){
+    print(x)
+    goplot <- xEnrichCompare(l, displayBy="fc", FDR.cutoff = 0.05, wrap.width = 45) +
+      scale_fill_brewer(palette='Set2') +
+      ggtitle('GO Enrichment Biological Process (FDR < 0.05)')
+  } else
+    goplot = NULL
+  return(goplot)
+}
+CP_plot = function(x){
+  l = enGo_CP_simple[getChild(x)]
+  l = l[!sapply(l, is.null)]
+  if(length(l) > 2){
+    print(x)
+    df = ldply(l, function(x) select(x@result, -geneID), .id = "Block") %>%
+      filter(p.adjust < 0.05)
+    goplot <- df %>%
+      ggplot(aes(Description, -log2(p.adjust))) +
+      geom_bar(stat="identity")  + coord_flip()
+    if(length(unique(df$Block))>1){
+      goplot <- goplot + facet_wrap(~Block, nrow = 1)
+    } else
+      goplot <- ifelse(is.na(df$Block[1]), NULL, goplot + ggtitle(df$Block[1]))
+  } else
+    goplot = NULL
+  return(goplot)
+}
+CP_print = function(x, fdr = 0.05, simple = FALSE, recursive=TRUE){
+  if(simple){
+    enGo = enGo_CP_simple
+  } else{
+    enGo = enGo_CP
+  }
+  if(recursive){
+    out = ldply(enGo[getChild(x)],
+          function(d) d@result %>%
+            dplyr::select(-geneID) %>%
+            filter(p.adjust < fdr), .id =  "Block")
+  } else
+    out = enGo[[x]]@result %>%
+      dplyr::select(-geneID) %>%
+      filter(p.adjust < fdr) %>%
+      mutate(Block = x) %>%
+      select(Block, everything())
+  return(out)
+}
 
+block_path = "data/output/SBM/clustering/head_weights-spearman_fdr-1e-06_mcmc_mode_hierarchical-SBM_gene-blocks"
+level = 1
+block  = 11
 block_summary = read.csv(file.path(block_path, "block_summary.csv"))
 block_summary$Name = block_summary$File %>% {gsub(".csv", "", .)} %>% {gsub("/", "", .)}
 block_summary$Parent = block_summary$Name %>%
@@ -98,7 +154,8 @@ block_summary$n_enrich_simple = laply(enGo_CP_simple,
                                function(x) select(x@result, p.adjust) %>%
                                  filter(p.adjust < 0.05) %>% nrow)
 
-table(block_summary$n_enrich[block_summary$Nested_Level==1]!=0)
+table_en = table(block_summary$n_enrich[block_summary$Nested_Level==1]!=0)
+table_en/sum(table_en)
 
 ggplot(filter(block_summary, Nested_Level == 1), aes(Assortatitvity, -log2(p.adjust), color = Parent)) +
   geom_point() + geom_label_repel(aes(label = Block))
@@ -117,7 +174,7 @@ ggplot(filter(block_summary, Nested_Level == 2), aes(Assortatitvity, -log2(p.adj
   geom_point() + geom_label_repel(aes(label = Block))
 ggplot(filter(block_summary, Nested_Level == 2), aes(N_genes, -log2(p.adjust), color = Parent)) +
   geom_point() + geom_label_repel(aes(label = Block))
-ggplot(filter(block_summary, Nested_Level == 2), aes(Assortatitvity, n_enrich, color = Parent)) +
+ggplot(filter(block_summary, Nested_Level == 2), aes(Assortatitvity, n_enrich/N_genes, color = Parent)) +
   geom_point() + geom_label_repel(aes(label = Block))+ geom_smooth(method = lm, color = "black")
 ggplot(filter(block_summary, Nested_Level == 2), aes(N_genes, n_enrich, color = Parent)) +
   geom_point() + geom_label_repel(aes(label = Block))
@@ -127,54 +184,17 @@ ggplot(filter(block_summary, Nested_Level == 2), aes(N_genes, n_enrich_simple, c
   geom_point() + geom_label_repel(aes(label = Block))
 
 
-go_1 = enGo_XGR[["1"]]
-go_0 = enGo_XGR[["0"]]
-xEnrichViewer(go_0, top_num = 10, sortBy = "adjp", details=F)
-l<- list(go_1, go_0); names(l) <- c("Block 1", "Block 0")
-
-parent = "4-0"
-getChild = function(parent, b_df = block_summary){
-  if(!parent %in% b_df$Name) stop("Parent block not found.")
-childs = b_df %>%
-  filter(Nested_Level == getLevel(parent)-1, Parent == strsplit(parent, "-")[[1]][1]) %>%
-  as.data.frame %>% `[[`("Name")
-  c(parent, childs)
-}
-
-
-goplot_list = llply(block_summary$Name[block_summary$Nested_Level==2], function(x) {
-  l = enGo_XGR[getChild(x)]
-  l = l[!sapply(l, is.null)]
-  if(length(l) > 2){
-    print(x)
-    goplot <- xEnrichCompare(l, displayBy="fc", FDR.cutoff = 0.05, wrap.width = 45) +
-      scale_fill_brewer(palette='Set2') +
-      ggtitle('GO Enrichment Biological Process (FDR < 0.05)')
-  } else
-    goplot = NULL
-  goplot
-})
-save_plot("go_head_level_4-0-0_XGR.png", goplot_list[[5]], base_height = 7, base_asp = 0.25, ncol=5)
+goplot_list = llply(block_summary$Name[block_summary$Nested_Level==3], XGR_plot)
+save_plot("go_head_level_11-1-0-super_translation.png", XGR_plot("11-1-0"), base_height = 7, base_asp = 0.25, ncol=4)
 
 l = enGo_CP_simple[block_summary$Name[block_summary$Nested_Level==3]]
 
-llply(block_summary$Name[block_summary$Nested_Level==2], function(x) {
-  l = enGo_CP[getChild(x)]
-  l = l[!sapply(l, is.null)]
-  if(length(l) > 2){
-    print(x)
-    df = ldply(l, function(x) select(x@result, -geneID), .id = "Block") %>%
-      filter(p.adjust < 0.05)
-    goplot <- df %>%
-      ggplot(aes(Description, -log2(p.adjust))) +
-      geom_bar(stat="identity")  + coord_flip()
-    if(length(unique(df$Block))>1){
-      goplot <- goplot + facet_wrap(~Block, nrow = 1)
-    } else
-      goplot <- ifelse(is.na(df$Block[1]), NULL, goplot + ggtitle(df$Block[1]))
-  } else
-    goplot = NULL
-  goplot
-})
+llply(block_summary$Name[block_summary$Nested_Level==2], CP_plot)
 
 save_plot("go_head_level_4-0-0_gueto.png", goplot, base_height = 9, base_asp = 0.25, ncol=5)
+
+Level = 3
+for(x in block_summary$Name[block_summary$Nested_Level==Level]){
+  df = CP_print(x)
+  print(df)
+}
