ecef394aad184b4b5b5b0727b84f0b48be3b8f3f
Diogo Melo <diogro@gmail.com>
2024-04-02 12:48:17 -0400

    bunch of networks for revised manuscript

--- SBM/generate_known_matrices.R
+++ SBM/generate_known_matrices.R
@@ -55,6 +55,152 @@ mod.cor[1:10, 1:10]
 save_plot("SBM/modular_matrix.pdf", corrPlot(mod.cor))
 save_plot("SBM/non_modular_matrix.pdf", corrPlot(non.mod.cor))
 
+
+## Figure 1 matrices for cool networks
+
+# matrix with 5 modules
+
+modules = matrix(c(rep(c(1, 0, 0, 0, 0), each = 10),
+                   rep(c(0, 1, 0, 0, 0), each = 10),
+                   rep(c(0, 0, 1, 0, 0), each = 10),
+                   rep(c(0, 0, 0, 1, 0), each = 10),
+                   rep(c(0, 0, 0, 0, 1), each = 10)), 50, 5)
+modules_z_coef = c(.1, 0.4, .4, .4, .4, 0.4)
+mod.cor.5 = calcExpectedMatrix(modules, modules_z_coef)
+
+# Replace values different than 1 with a random value centered on the current value
+mod.cor.5[mod.cor.5 != 1] = rnorm(sum(mod.cor.5 != 1), mean = mod.cor.5[mod.cor.5 != 1], sd = 0.1)
+# Set a random subsetd of the smaller values with 0
+N = sum(mod.cor.5 < 0.3) / 1.1
+mod.cor.5[sample(which(mod.cor.5 < 0.3), N)] = 0
+
+N = sum(mod.cor.5 > 0.3 & mod.cor.5 < 0.45) / 2.15
+mod.cor.5[sample(which(mod.cor.5 > 0.3 & mod.cor.5 < 0.45), N)] = 0
+
+# Make matrix symmetric
+mod.cor.5[lower.tri(mod.cor.5)] = t(mod.cor.5)[lower.tri(mod.cor.5)]
+write.csv(mod.cor.5, "SBM/network_architecture/modular_matrix_5modules.csv")
+
+for(m in 1:5){
+    index = (m-1)*10 + 1:10
+    n_index = index[order(colSums(mod.cor.5[index, index]), decreasing = TRUE)]
+    mod.cor.5[index, index] = 
+        mod.cor.5[n_index, n_index]
+}
+save_plot("SBM/network_architecture/modular_matrix_5modules.png", corrPlot(mod.cor.5) + theme_void() + theme(legend.position = "none"))
+
+# Core-periphery matrix
+
+modules = matrix(c(rep(c(1, 0, 0, 0, 0), each = 10),
+                   rep(c(1, 1, 1, 1, 1), each = 10),
+                   rep(c(0, 1, 1, 1, 1), each = 10),
+                   rep(c(0, 0, 1, 1, 1), each = 10),
+                   rep(c(0, 0, 0, 1, 1), each = 10),
+                   rep(c(0, 0, 0, 0, 1), each = 10)), 50, 6)
+modules_z_coef = c(.45, 
+                   .2, -.2, -.1, -0.1, -0.05, -0.05)
+mod.cor.cp = calcExpectedMatrix(modules, modules_z_coef)
+
+# Replace values different than 1 with a random value centered on the current value
+mod.cor.cp[mod.cor.cp != 1] = rnorm(sum(mod.cor.cp != 1), mean = mod.cor.cp[mod.cor.cp != 1], sd = 0.1)
+# Set a random subsetd of the smaller values with 0
+N = sum(mod.cor.cp < 0.3) / 1.2
+mod.cor.cp[sample(which(mod.cor.cp < 0.3), N)] = 0
+
+# N = sum(mod.cor.cp > 0.35) / 2.5
+# mod.cor.cp[sample(which(mod.cor.cp > 0.35), N)] = 0
+
+# Make matrix symmetric
+mod.cor.cp[lower.tri(mod.cor.cp)] = t(mod.cor.cp)[lower.tri(mod.cor.cp)]
+write.csv(mod.cor.cp, "SBM/network_architecture/cp_matrix.csv")
+
+for(m in 1:5){
+    index = (m-1)*10 + 1:10
+    n_index = index[order(colSums(mod.cor.cp[index, index]), decreasing = TRUE)]
+    mod.cor.cp[index, index] = 
+        mod.cor.cp[n_index, n_index]
+}
+diag(mod.cor.cp) = 1
+save_plot("SBM/network_architecture/cp_matrix.png", corrPlot(mod.cor.cp) + theme_void() + theme(legend.position = "none"))
+
+
+
+# Disassortative matrix
+
+modules = matrix(c(rep(c(1, 0, 0, 0, 0), each = 10),
+                   rep(c(0, 1, 0, 0, 0), each = 10),
+                   rep(c(0, 0, 1, 0, 0), each = 10),
+                   rep(c(0, 0, 0, 1, 0), each = 10),
+                   rep(c(0, 0, 0, 0, 1), each = 10)), 50, 6)
+modules_z_coef = c(.5, 
+                   -.5, -.5, -.5, -0.5, -0.5)
+mod.cor.dis = calcExpectedMatrix(modules, modules_z_coef)
+
+for(i in 1:20){
+    for(j in 21:50){
+        mod.cor.dis[i, j] = 0
+    }
+}
+
+for(i in 31:40){
+    for(j in 41:50){
+        mod.cor.dis[i, j] = 0
+    }
+}
+
+for(i in 11:20){
+    for(j in 41:50){
+        mod.cor.dis[i, j] = 0.46
+    }
+}
+
+# Replace values different than 1 with a random value centered on the current value
+mod.cor.dis[mod.cor.dis != 1] = rnorm(sum(mod.cor.dis != 1), mean = mod.cor.dis[mod.cor.dis != 1], sd = 0.1)
+# Set a random subsetd of the smaller values with 0
+N = sum(mod.cor.dis < 0.3) / 1.2
+mod.cor.dis[sample(which(mod.cor.dis < 0.3), N)] = 0
+
+ N = sum(mod.cor.dis > 0.35) / 2.5
+ mod.cor.dis[sample(which(mod.cor.dis > 0.35), N)] = 0
+
+# Make matrix symmetric
+mod.cor.dis[lower.tri(mod.cor.dis)] = t(mod.cor.dis)[lower.tri(mod.cor.dis)]
+write.csv(mod.cor.dis, "SBM/network_architecture/dis_matrix.csv")
+
+for(m in 1:5){
+    index = (m-1)*10 + 1:10
+    n_index = index[order(colSums(mod.cor.dis[index, index]), decreasing = TRUE)]
+    mod.cor.dis[index, index] = 
+        mod.cor.dis[n_index, n_index]
+}
+diag(mod.cor.dis) = 1
+save_plot("SBM/network_architecture/dis_matrix.png", corrPlot(mod.cor.dis) + theme_void() + theme(legend.position = "none"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ## WGCNA
 
 dissTOM = 1 - TOMsimilarity(mod.cor^2)
